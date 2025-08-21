b94488706e013e201db36a2d5d869239620f73d6
Brett Ammeson <ammesonb@gmail.com>
2025-08-21 01:14:47 -0400

    Comments, for easier understanding/test writing later

--- src/logistics/graph-solver.ts
+++ src/logistics/graph-solver.ts
@@ -25,6 +25,9 @@ interface RecipeNode {
   outputs: RecipeLink[]
 }
 
+/**
+ * Generate a blank recipe node for the given recipe.
+ */
 const newRecipeNode = (
   recipe: Recipe,
   ingredients: RecipeIngredient[],
@@ -40,10 +43,18 @@ const newRecipeNode = (
   outputs: [],
 })
 
+/**
+ * Determine the PRODUCED quantity of a recipe's ingredient.
+ * This is used to calculate the external amount needed as an input for the recipe to be sustainable.
+ */
 const getCatalystQuantity = (ingredient: RecipeIngredient, recipe: RecipeNode) => {
   return recipe.products.find((prod) => prod.item === ingredient.item)?.amount || 0
 }
 
+/**
+ * Identify which and what quantity is needed from available input sources to craft the recipe.
+ * If any ingredient cannot be satisfied, returns an empty list immediately.
+ */
 const getRecipeLinks = (
   recipe: RecipeNode,
   producedRecipes: Record<string, RecipeNode>,
@@ -98,6 +109,19 @@ const getRecipeLinks = (
   return links
 }
 
+/**
+ * Select an optimal set of input sources for a required amount of an ingredient.
+ *
+ * Prefers to consume:
+ * - crafted sources over natural
+ * - closest single-source that can fully satisfy the required amount
+ * - smallest quantity outputs when multiple sources are needed, to leave largest products for later consumers
+ *     - The idea is this way other recipes may only need a single source, if this one already requires multiple.
+ *       So this source eats up all the bits and pieces, and other recipes are more likely to get a single, clean source.
+ *
+ * If insufficient quantity is available, returns an empty list.
+ * Natural resources are allowed to have a remainder, which will be filled from an infinite "mined" source.
+ */
 const selectIngredientSources = (
   ingredient: RecipeIngredient,
   amountNeeded: number,
@@ -154,17 +178,22 @@ const produceRecipe = (recipe: RecipeNode, batchNumber: number, inputs: RecipeLi
   recipe.inputs = inputs
 }
 
+/*
+ * Decrement the available products of recipes used in the given links, then update their fullyConsumed status accordingly.
+ */
 const decrementConsumedProducts = (
   recipesByName: Record<string, RecipeNode>,
   links: RecipeLink[],
 ) => {
   for (const link of links) {
     const sourceNode = recipesByName[link.source]
+    // add the output link, for a double-headed list structure approach
     sourceNode.outputs.push(link)
     const productIndex = sourceNode.availableProducts.findIndex((p) => p.item === link.material)
     if (productIndex >= 0) {
       sourceNode.availableProducts[productIndex].amount -= link.amount
     } else {
+      // should never be able to make a link for something without the product, but here as a safeguard
       throw new Error(
         `Unable to find product ${link.material} in source ${link.source} - should never happen!`,
       )
@@ -176,6 +205,18 @@ const decrementConsumedProducts = (
   }
 }
 
+/**
+ * Figures out recipe production chain in graph-style (breadth-first) manner.
+ *
+ * While pre-processing to try to group recipes ahead of time could be more optimal, then we have to go through everything twice
+ * as the first pass likely cannot deal with the nuance of quantities and mutual dependencies.
+ * This approach, while possibly requiring more processing time, is simpler to understand and implement.
+ *
+ * 1. From a list of currently-produced recipes (including natural resources), see if we have enough to produce any given recipe
+ * 2. If we can produce it, then add it to this batch and decrement available products to avoid over-consumption.
+ * 3. Identify and add any recipes that are codependent, where all ingredients are satisified except for a mutual dependency.
+ * 4. At end of batch, flip recipes from pending -> produced for next iteration
+ */
 export const solveRecipeChain = (rawRecipes: string[]): RecipeNode[] => {
   const data = useDataStore()
 
@@ -215,6 +256,7 @@ export const solveRecipeChain = (rawRecipes: string[]): RecipeNode[] => {
     // TODO: this will be done by seeing if another recipe being processed satisfies this node, IFF this node satisfies that node too
 
     pendingRecipes = pendingRecipes.filter((recipe) => !batchRecipes.includes(recipe))
+    // wait to add recipes to produced until AFTER batch, to get proper tiering/sorting
     Object.assign(
       producedRecipes,
       Object.fromEntries(batchRecipes.map((recipe) => [recipe.recipe.name, recipe])),
