ac86c18ce0f7194b7cc04fec111682a98390c6a6
Brett Ammeson <ammesonb@gmail.com>
2025-08-28 23:41:49 -0700

    Component reorg

--- src/components/modals/FloorEditModal.vue
+++ src/components/modals/FloorEditModal.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue'
 import { useFactoryStore } from '@/stores/factory'
-import IconSelector from '@/components/IconSelector.vue'
 
 interface Props {
   show: boolean
