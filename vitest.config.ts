3be4618abf3230680ffadadc5ad63bc267becfe7
Brett Ammeson <ammesonb@gmail.com>
2025-09-11 16:29:31 -0700

    Adds setup for UI component testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,21 @@
 import { fileURLToPath } from 'url'
 import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue()],
   test: {
     environment: 'happy-dom',
     globals: true,
-    setupFiles: ['./src/logistics/__tests__/test-setup.ts'],
+    setupFiles: [
+      './src/logistics/__tests__/test-setup.ts',
+      './src/components/__tests__/component-setup.ts',
+    ],
+    server: {
+      deps: {
+        inline: ['vuetify'],
+      },
+    },
   },
   resolve: {
     alias: {
